7b155619ef453ae9e23ea3bfcaa45cbb7f3b9cdc
admblack97 <admblack97@gmail.com>
2018-03-19 14:14:17 +0800

    komat kamit

---
+++ A01/src/a01/Mahasiswa.java
@@ -0,0 +1,30 @@
+package a01;
+
+public class Mahasiswa {
+    //atribut
+    String NPM;
+    String nama;
+    double IPK;
+    char kelas;
+    char nilaiPBO1;
+    boolean statusAktif;
+    
+    //behavior
+    void infoMhs () {
+        System.out.println("NPM     :"+NPM);
+        System.out.println("Nama    :"+nama);
+        System.out.println("IPK     :"+IPK);
+        System.out.println("Kelas   :"+kelas);
+        System.out.println("Nilai   :"+nilaiPBO1);
+        System.out.println("Status  :"+statusAktif);
+        
+    }
+    
+    void isiAbsen (){
+        
+    }
+    
+    void ubahNilai () {
+        
+    }
+}
