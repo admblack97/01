8c3714862c3caf48743a5c66b14f173b065db0eb
admblack97 <admblack97@gmail.com>
2018-03-19 14:15:15 +0800

    mengisi atribut objek

---
+++ A01/src/a01/MahasiswaBeraksi.java
@@ -0,0 +1,19 @@
+package a01;
+
+public class MahasiswaBeraksi {
+    public static void main(String[] args) {
+        //ini adalah instance (pembuatan objek dengan nama objek
+        Mahasiswa m1 = new Mahasiswa();
+        
+        //mengisi atribut objek
+        m1.NPM = "16.63.0007";
+        m1.nama = "AAD";
+        m1.IPK = 3.2;
+        m1.kelas = 'A';
+        m1.nilaiPBO1 = 'A';
+        m1.statusAktif = true;
+        
+        //menjalankan method objek
+        m1.infoMhs();
+    }
+}
